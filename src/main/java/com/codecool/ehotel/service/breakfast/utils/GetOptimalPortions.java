4c8ac51dd5772365a2179850a3aeba3d76119449
Daniel <dani.marinescu7@gmail.com>
2023-07-13 23:23:42 +0300

    Refactored getOptimalPortions

--- src/main/java/com/codecool/ehotel/service/breakfast/utils/GetOptimalPortions.java
+++ src/main/java/com/codecool/ehotel/service/breakfast/utils/GetOptimalPortions.java
@@ -1,35 +1,26 @@
 package com.codecool.ehotel.service.breakfast.utils;
 
-import com.codecool.ehotel.model.Buffet;
 import com.codecool.ehotel.model.GuestType;
 import com.codecool.ehotel.model.MealType;
 import com.codecool.ehotel.service.buffet.BuffetService;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class GetOptimalPortions {
     public static List<BuffetService.RefillRequest> getOptimalPortions(Map<GuestType, Integer> guestsToExpect, int cyclesLeft, double costOfUnhappyGuest) {
-        List<BuffetService.RefillRequest> refillRequests = new ArrayList<>();
+        return guestsToExpect.entrySet().stream()
+                .flatMap(entry -> {
+                    GuestType guestType = entry.getKey();
+                    List<MealType> mealTypes = guestType.getMealPreferences();
+                    Integer numberOfGuests = entry.getValue();
+                    Optional<MealType> cheapestMeal = mealTypes.stream()
+                            .min(Comparator.comparingDouble(MealType::getMealCost));
 
-        for (Map.Entry<GuestType, Integer> entry : guestsToExpect.entrySet()) {
-            GuestType guestType = entry.getKey();
-            List<MealType> mealTypes = guestType.getMealPreferences();
-            Integer numberOfGuests = entry.getValue();
-            Optional<MealType> cheapestMeal = mealTypes.stream()
-                    .min(Comparator.comparingDouble(MealType::getMealCost));
-
-            if (cheapestMeal.isPresent()) {
-                MealType mealWithLowestCost = cheapestMeal.get();
-                if (cyclesLeft > 3) {
-                    if (mealWithLowestCost.getMealCost() < costOfUnhappyGuest) {
-                        refillRequests.add(new BuffetService.RefillRequest(mealWithLowestCost, numberOfGuests));
-                    }
-                } else {
-                    refillRequests.add(new BuffetService.RefillRequest(mealWithLowestCost, numberOfGuests));
-                }
-            }
-        }
-
-        return refillRequests;
+                    return cheapestMeal.stream()
+                            .filter(meal -> cyclesLeft > 3 || meal.getMealCost() < costOfUnhappyGuest)
+                            .map(meal -> new BuffetService.RefillRequest(meal, numberOfGuests));
+                })
+                .collect(Collectors.toList());
     }
 }

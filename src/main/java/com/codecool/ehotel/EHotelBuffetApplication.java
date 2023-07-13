67bf486ff3c37e0c2f03d5abddea7aa9e75498c9
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-14 00:59:38 +0300

    create Logger interface and ConsoleLogger class

--- src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
+++ src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
@@ -1,9 +1,15 @@
 package com.codecool.ehotel;
 
+import com.codecool.ehotel.logging.ConsoleLogger;
+import com.codecool.ehotel.logging.Logger;
+
 public class EHotelBuffetApplication {
 
     public static void main(String[] args) {
-
+        // Initialize logger
+        Logger logger = new ConsoleLogger();
+        logger.logInfo("Starting EHotel Buffet Application");
+        logger.logError("This is an error message");
         // Initialize services
 
 

7fc9481796bde748794c888950b9922415b55825
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-18 14:34:37 +0300

    refactor display guests in console

--- src/main/java/com/codecool/ehotel/model/MealType.java
+++ src/main/java/com/codecool/ehotel/model/MealType.java
@@ -3,17 +3,10 @@ package com.codecool.ehotel.model;
 import static com.codecool.ehotel.model.MealDurability.*;
 
 public enum MealType {
-    SCRAMBLED_EGGS (70, SHORT),
-    SUNNY_SIDE_UP (70, SHORT),
-    FRIED_SAUSAGE(100, SHORT),
-    FRIED_BACON(70, SHORT),
-    PANCAKE(40, SHORT),
-    CROISSANT(40, SHORT),
-    MASHED_POTATO(20, MEDIUM),
-    MUFFIN(20, MEDIUM),
-    BUN(10, MEDIUM),
-    CEREAL(30, LONG),
-    MILK(10, LONG);
+    SCRAMBLED_EGGS(70, SHORT), SUNNY_SIDE_UP(70, SHORT),
+    FRIED_SAUSAGE(100, SHORT), FRIED_BACON(70, SHORT), PANCAKE(40, SHORT),
+    CROISSANT(40, SHORT), MASHED_POTATO(20, MEDIUM), MUFFIN(20, MEDIUM),
+    BUN(10, MEDIUM), CEREAL(30, LONG), MILK(10, LONG);
 
     private int cost;
     private MealDurability mealDurability;

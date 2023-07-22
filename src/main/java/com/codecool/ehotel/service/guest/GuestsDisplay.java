4864ce54bb751a5153e18f1b7a79f371f76f340a
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-22 18:11:21 +0300

    updated guests diplay

--- src/main/java/com/codecool/ehotel/service/guest/GuestsDisplay.java
+++ src/main/java/com/codecool/ehotel/service/guest/GuestsDisplay.java
@@ -9,6 +9,11 @@ import java.util.List;
 public class GuestsDisplay {
     public static void displayGuests(List<Guest> guests) {
         out.println("Guests:");
+        if(guests.isEmpty()) {
+            out.println("No guests for this cycle.");
+            out.println();
+            return;
+        }
         for (Guest guest : guests) {
             out.println(String.format("- %s (%s): Booked from %s to %s",
                     guest.name(), guest.guestType(), guest.checkIn(), guest.checkOut()));

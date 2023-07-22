35f5bb7b34ce362eaf6e95f81f5302460c2ec58e
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-22 20:23:37 +0300

    delete unused methods from Buffet class

--- src/main/java/com/codecool/ehotel/model/Buffet.java
+++ src/main/java/com/codecool/ehotel/model/Buffet.java
@@ -33,18 +33,4 @@ public class Buffet {
     public Map<MealType, List<MealPortion>> getMealPortionsMap() {
         return mealPortionsMap;
     }
-
-    public int estimateMaxPortions() {
-        int totalCapacity = 0;
-        for (List<MealPortion> mealPortions : mealPortionsMap.values()) {
-            totalCapacity += mealPortions.size();
-        }
-        return totalCapacity;
-    }
-
-    public int estimateMaxPortionsToAdd(int maxCapacity) {
-        int currentCapacity = estimateMaxPortions();
-        int maxPortionsToAdd = Math.max(0, maxCapacity - currentCapacity);
-        return maxPortionsToAdd;
-    }
 }
